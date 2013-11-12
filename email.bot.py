5c9f16a0c9021608240e9c01f768cdd551e0965f
Nimphina <rune80@hotmail.co.uk>
2013-11-12 20:26:21 +0000

    Complete re-write because imaplib is dumb
    New library dedicated for Gmail and produces nice output

--- email.bot.py
+++ email.bot.py
@@ -1,121 +1,59 @@
-import imaplib
-import os
-import sys
-import pprint
+import gmail, os, sys, time
+#Gmail - https://github.com/charlierguo/gmail	
 
-#imaplib.Debug = 4
+def gm_connect():
 
-def open_connection(verbose=False):
+	username 		= "nimsphina@gmail.com" #Send me some emails sure
+	password		= ""
 
-    hostname = "imap.gmail.com"
-    port = 993
-    username = "nimsphina"
-    password = ""
+	try:
+		print "Logging into {0}".format(username)
+		gmail_connection = gmail.login(username, password)
+		return gmail_connection
+	except:
+		print "Error", sys.exc_info()[0:5]
+		gmail_connection.logout()
 
-    connection = imaplib.IMAP4_SSL(hostname, port)
+def get_email(g_conn):
 
-    # Login to our account
-    if verbose: print 'Logging in as', username
-    connection.login(username, password)
-    return connection
+	email_subject 		= "NULL"
+	email_link			= "NULL"
+	final_array			= []
 
-def get_email_sender(conn, email_num):
-    
-    try:
-            conn.select('INBOX', readonly=True) #open Inbox folder for reading
+	unread_emails = g_conn.inbox().mail(unread=True, sender="noreply@curse.com") #Get all unread mail from target address
+	
+	counter = 0
 
-            typ, msg_data = conn.fetch(str(email_num), '(BODY.PEEK[HEADER])') #Fetch email titles
+	print "Fetching new emails, may take a while if there is a lot of them"
 
-            for response_part in msg_data:
+	for email in unread_emails: #Read through list of mail
+		unread_emails[counter].fetch() #Fetch email content
 
-                if isinstance(response_part, tuple):
-                    counter = 0
-                    return_string = "Null"
-                    email_title = response_part[1].split()
-                    
-                    for item in email_title:
-                        #print item
-                        if item == "From:":
-                            return_string = ""
-                            while email_title[counter +1] != "To:":
-                                return_string = return_string + email_title[counter +1] + " "
-                                counter +=1
-                            #return return_string
-                        counter += 1
-                    return return_string
+		email_subject = unread_emails[counter].subject #Get subject
 
-    except: #If error, logout
-        return "error"
+		email_body = unread_emails[counter].body #Fetch emailbody and assign a variable
+		email_array = email_body.split() #Split body into array
 
-def get_email_subject(conn, email_num):
-    
-    try:
-        conn.select('INBOX', readonly=True) #open Inbox folder for reading
-        
-        typ, msg_data = conn.fetch(str(email_num), '(BODY.PEEK[HEADER])') #Fetch email titles
-        
-        for response_part in msg_data:
-            #print response_part[1]
-            if isinstance(response_part, tuple):
-                counter = 0
-                return_string = "Null"
-                email_title = response_part[1].split()
-                
-                for item in email_title:
-                    
-                    if item == "Subject:":
-                        return_string = ""
-                        while email_title[counter +1] != "Content-type:":
-                            return_string = return_string + email_title[counter +1] + " "
-                            counter +=1
-                        #return return_string
-                    counter += 1
-                return return_string
+		for item in email_array: 
+			if "http://www.minecraftforum.net/topic/" in item:
+				email_link = item
 
-    except: #If error, logout
-        print "Unexpected error:", sys.exc_info()[0]
-        return "error"
-        
+		email.read() #For when the bot is deployed
 
-def get_link(): #This function needs to delete read mail
-    pass
+		counter += 1
+		final_array.append("{0} - {1}".format(email_subject, email_link))
 
-def read_emails(conn, option, email_no):
-    
-    #Declare needed variables
-    email_subject       = "NULL"
-    email_link          = "NULL"
-    email_post_sub      = "NULL"
-    return_array        = []
+	return final_array 
 
-    typ, msg_data = conn.select('INBOX', readonly=True)
+try:
+	
+	g_conn = gm_connect()
 
-    no_of_emails = int(msg_data[0])
+	list_of_topics = get_email(g_conn)
 
-    for x in range(no_of_emails):
-        x+=1
-        email_post_sub = get_email_sender(conn, x)
-        print email_post_sub
-        if "\"Minecraft Forum\" <noreply@curse.com>" in email_post_sub:
-            email_subject = get_email_subject(conn, x)
-            return_array.append("{0}- {1}".format(email_subject, email_link))
+	for item in list_of_topics:
+		
+		print item
 
-    if email_subject == "error":
-        print "An error occured while trying to get the email_subject"
-        return "NULL"
-
-    else:
-        return return_array
-
-#main function
-if __name__ == '__main__':
-    c = open_connection(verbose=True)
-
-    try:
-       
-       post_array = read_emails(c, "last", 0)
-       for item in post_array:
-            print item     
-                        
-    finally:
-        c.logout()
+finally:
+	g_conn.logout()
